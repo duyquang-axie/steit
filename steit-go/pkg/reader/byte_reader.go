b5138d99f3cdafd2699fa72e02fa42394d1fd4df
Truong Minh Nhat Quang <nhatquangsin@gmail.com>
2022-02-25 16:33:02 +0700

    Add byte reader

--- steit-go/pkg/reader/byte_reader.go
+++ steit-go/pkg/reader/byte_reader.go
@@ -1,5 +1,63 @@
 package reader
 
-func NewByteReader([]byte) IReader {
-	return nil
+var _ IReader = (*ByteReader)(nil)
+
+type ByteReader struct {
+	bytes []byte
+	offset int
 }
+
+func NewByteReader(bytes []byte) IReader {
+	return &ByteReader{bytes: bytes, offset: 0}
+}
+
+func (b *ByteReader) Remaining() int {
+	return len(b.bytes) - b.offset
+}
+
+func (b *ByteReader) ReadUint8() byte {
+	if b.Remaining() <= 0 {
+		panic("end of stream")
+	}
+
+	n := b.bytes[b.offset]
+	b.offset = b.offset + 1
+
+	return n
+}
+
+func (b *ByteReader) Read(count int) []byte {
+	if b.Remaining() <= 0 {
+		panic("end of stream")
+	}
+
+	var bytes []byte
+
+	for i := 0; i < count; i++ {
+		bytes[i] = b.bytes[b.offset]
+		b.offset = b.offset + 1
+	}
+
+	return bytes
+}
+
+func (b *ByteReader) ReadKey() (uint32, interface{}) {
+	panic("implement me")
+}
+
+func (b *ByteReader) ReadUint32() uint32 {
+	panic("implement me")
+}
+
+func (b *ByteReader) EndOfStream() bool {
+	panic("implement me")
+}
+
+func (b *ByteReader) GetNested() IReader {
+	panic("implement me")
+}
+
+func (b *ByteReader) SkipField(i int) {
+	panic("implement me")
+}
+
--- steit-go/pkg/reader/ireader.go
+++ steit-go/pkg/reader/ireader.go
@@ -3,7 +3,10 @@ package reader
 import "github.com/axieinfinity/steit-go/pkg/codec"
 
 type IReader interface {
+	Remaining() int
 	ReadUint32() uint32
+	ReadUint8() byte
+	Read(int) []byte
 	EndOfStream() bool
 	ReadKey() (uint32, codec.WireType)
 	GetNested() IReader
